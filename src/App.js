3c96d47332610eb2c3a3105ba64f4144fb92733d
HossamSalaheldeen <hossam.salah1995@gmail.com>
2021-07-14 06:41:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Container, Row, Col } from "reactstrap";
+import "./styles.css";
+import MasterForm from './components/MasterForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <style>{"body { background-color: lightgray; }"}</style>
+      </Helmet>
+      <Container>
+        <Row>
+          <Col>
+            <MasterForm />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
